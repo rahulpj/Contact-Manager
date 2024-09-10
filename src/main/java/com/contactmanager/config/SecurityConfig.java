adeee82b69eef0c4935783d7b40b3959f172ac9c
Rahul Panjwani <rahulpanjwani2003@gmail.com>
2024-09-11 02:26:59 +0530

    implemented basic register and login

--- src/main/java/com/contactmanager/config/SecurityConfig.java
+++ src/main/java/com/contactmanager/config/SecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.contactmanager.services.impl.SecurityCustomUserDetailService;
 
@@ -17,6 +20,7 @@ public class SecurityConfig {
     @Autowired
     SecurityCustomUserDetailService securityCustomUserDetailService;
 
+    // Configuration of Authentication Provider
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
@@ -25,6 +29,25 @@ public class SecurityConfig {
         // password encoder ka object
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        //configuration
+
+        //configure urls that which one should be public and which should be secured
+        httpSecurity.authorizeHttpRequests(authorize->{
+            // authorize.requestMatchers("/home","/register","/services").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        
+        });
+
+        // form default login
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
     }
 
 
--- src/main/java/com/contactmanager/entities/User.java
+++ src/main/java/com/contactmanager/entities/User.java
@@ -48,7 +48,7 @@ public class User implements UserDetails{
     private String profilePic;
     private String phoneNumber;
     @Getter(value = AccessLevel.NONE)
-    private boolean enabled=false;
+    private boolean enabled=true;
     private boolean emailVerified=false;
     private boolean phoneVerified=false;
 
