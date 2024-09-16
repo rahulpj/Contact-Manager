579eab6e3f7fc6f25caa10ff5c8467ec1bae691a
Rahul Panjwani <rahulpanjwani2003@gmail.com>
2024-09-16 22:02:32 +0530

    modified .gitignore

--- src/main/java/com/contactmanager/ContactManagerApplication.java
+++ src/main/java/com/contactmanager/ContactManagerApplication.java
@@ -3,10 +3,18 @@ package com.contactmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class ContactManagerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
 		SpringApplication.run(ContactManagerApplication.class, args);
 	}
 
